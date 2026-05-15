388f7aa02436a3cd33f716e8530cd5ce0744f5ba
Timothy Riordan <tmiker2004@yahoo.com>
2026-05-14 21:21:05 -0500

    Products.Write.API: reset local sql connection string.

--- Products.Write.Infrastructure/Migrations/20260515021741_ContextModelUpdate.Designer.cs
+++ Products.Write.Infrastructure/Migrations/20260515021741_ContextModelUpdate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Products.Write.Infrastructure.DataAccess;
 
@@ -11,9 +12,11 @@ using Products.Write.Infrastructure.DataAccess;
 namespace Products.Write.Infrastructure.Migrations
 {
     [DbContext(typeof(EventStoreDbContext))]
-    partial class EventStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260515021741_ContextModelUpdate")]
+    partial class ContextModelUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Products.Write.Infrastructure/Migrations/20260515021741_ContextModelUpdate.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Products.Write.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ContextModelUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CorrelationId",
+                schema: "dbo",
+                table: "Product Event Records",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CorrelationId",
+                schema: "dbo",
+                table: "Outbox Records",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CorrelationId",
+                schema: "dbo",
+                table: "Product Event Records",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CorrelationId",
+                schema: "dbo",
+                table: "Outbox Records",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
