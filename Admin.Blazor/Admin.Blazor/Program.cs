3c5cea021cc6f7718b1fe8ac9fa197b8e9624402
Timothy Riordan <tmiker2004@yahoo.com>
2026-06-04 06:32:17 -0500

    Admin Client: removed commented and dev code from Storefront checkout view.

--- Admin.Blazor/Admin.Blazor/Program.cs
+++ Admin.Blazor/Admin.Blazor/Program.cs
@@ -192,7 +192,7 @@ app.MapRazorComponents<App>()
     .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(typeof(Admin.Blazor.Client._Imports).Assembly);
 
-app.MapGet("/ping", () => "Blazor Admin Client is up and running!  Last Build: 1 Jun 2026 @ 20:20 CST").AllowAnonymous();
+app.MapGet("/ping", () => "Blazor Admin Client is up and running!  Last Build: 4 Jun 2026 @ 06:30 CST").AllowAnonymous();
 
 app.MapGet("/login", (string? returnUrl, HttpContext httpContext) =>
 {
