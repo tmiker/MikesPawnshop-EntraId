22b4607a8c92220b00fa65b128109f6c002a8fdb
Timothy Riordan <tmiker2004@yahoo.com>
2026-06-14 12:40:49 -0500

    Updated Nuget Packages for all projects, and updated Admin Cliient ProjectNotes.razor.  Removed Duende IDP project.

--- Admin.Blazor/Admin.Blazor/Program.cs
+++ Admin.Blazor/Admin.Blazor/Program.cs
@@ -205,7 +205,7 @@ app.MapRazorComponents<App>()
     .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(typeof(Admin.Blazor.Client._Imports).Assembly);
 
-app.MapGet("/ping", () => "Blazor Admin Client is up and running!  Last Build: 9 Jun 2026 @ 01:40 CST").AllowAnonymous();
+app.MapGet("/ping", () => "Blazor Admin Client is up and running!  Last Build: 14 Jun 2026 @ 12:40 CST").AllowAnonymous();
 
 app.MapGet("/login", (string? returnUrl, HttpContext httpContext) =>
 {
