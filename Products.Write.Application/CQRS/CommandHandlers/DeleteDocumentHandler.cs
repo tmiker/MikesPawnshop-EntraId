f02b498b5c0dc2de37ddfd25f4c016f642b814f0
Timothy Riordan <tmiker2004@yahoo.com>
2026-07-20 01:08:19 -0500

    try catch added to Products Write API Delete Image and Delete Document Handlers.  Admin Client ProductDetail.razor in Components/Pages/ProductsWrite:  check if Images and Documents collection has any entries in ReloadAsync() method before attempt to assign primary image or item count.

--- Products.Write.Application/CQRS/CommandHandlers/DeleteDocumentHandler.cs
+++ Products.Write.Application/CQRS/CommandHandlers/DeleteDocumentHandler.cs
@@ -25,32 +25,39 @@ namespace Products.Write.Application.CQRS.CommandHandlers
 
         public async Task<DeleteDocumentResult> Handle(DeleteDocument command, CancellationToken cancellationToken)
         {
-            Product product = await _productRepository.GetProductByIdAsync(command.ProductId);
-
-            // delete image in domain
-            product.DeleteDocument(command.FileName, command.CorrelationId);
-            bool success = await _productRepository.SaveAsync(product);
-
-            if (success)
+            try
             {
-                // invoke domain events to update read side data store
-                if (product.DomainEvents is not null && product.DomainEvents.Any())
+                Product product = await _productRepository.GetProductByIdAsync(command.ProductId);
+
+                // delete image in domain
+                product.DeleteDocument(command.FileName, command.CorrelationId);
+                bool success = await _productRepository.SaveAsync(product);
+
+                if (success)
                 {
-                    foreach (var domainEvent in product.DomainEvents)
+                    // invoke domain events to update read side data store
+                    if (product.DomainEvents is not null && product.DomainEvents.Any())
                     {
-                        // publish the event to the bus
-                        _eventAggregator.Raise(domainEvent);
+                        foreach (var domainEvent in product.DomainEvents)
+                        {
+                            // publish the event to the bus
+                            _eventAggregator.Raise(domainEvent);
+                        }
                     }
+
+                    // delete image from azure blob storage
+                    string containerName = $"product-{command.ProductId}";
+                    string fileName = command.FileName;
+                    (bool IsSuccess, string? ErrorMessage) result = await _azureStorageService.DeleteProductDocumentFromAzureAsync(containerName, fileName, cancellationToken);
+                    return new DeleteDocumentResult(result.IsSuccess, result.ErrorMessage);
                 }
 
-                // delete image from azure blob storage
-                string containerName = $"product-{command.ProductId}";
-                string fileName = command.FileName;
-                (bool IsSuccess, string? ErrorMessage) result = await _azureStorageService.DeleteProductDocumentFromAzureAsync(containerName, fileName, cancellationToken);
-                return new DeleteDocumentResult(result.IsSuccess, result.ErrorMessage);
+                return new DeleteDocumentResult(success, "Error removing document from write side data store");
             }
-
-            return new DeleteDocumentResult(success, "Error removing document from write side data store");
+            catch (Exception ex)
+            {
+                return new DeleteDocumentResult(false, ex.Message);
+            }
         }
     }
 }
