ee84b0ba8a500ff29e9c7330de12def6f0fc391f
Timothy Riordan <tmiker2004@yahoo.com>
2026-07-26 15:24:11 -0500

    Blazor Consumer Client:  IProductsReadHttpService : ProductsReadApiService - commented out methods that are exclusively handled by the IPublicProductsReadHttpService implementation.

--- Consumer.Blazor/Consumer.Blazor.Client/Abstractions/IProductsReadHttpService.cs
+++ Consumer.Blazor/Consumer.Blazor.Client/Abstractions/IProductsReadHttpService.cs
@@ -7,12 +7,12 @@ namespace Consumer.Blazor.Client.Abstractions
     public interface IProductsReadHttpService
     {
         // IAsyncEnumerable<ProductDTO> StreamProductsAsync();
-        Task<(bool IsSuccess, IEnumerable<ProductDTO>? Products, string? ErrorMessage)> GetProductsAsync();
-        Task<(bool IsSuccess, IEnumerable<ProductSummaryDTO>? ProductSummaries, string? ErrorMessage)> GetProductSummariesAsync();
-        Task<(bool IsSuccess, IEnumerable<ProductDTO>? Products, PaginationMetadata? PagingData, DateTime? FetchTime, string? ErrorMessage)> GetPagedAndFilteredProductsAsync(string? filter, string? category, string? sortColumn, int pageNumber = 1, int pageSize = 10);
-        Task<(bool IsSuccess, IEnumerable<ProductSummaryDTO>? Products, PaginationMetadata? PagingData, DateTime? FetchTime, string? ErrorMessage)> GetPagedAndFilteredProductSummariesAsync(string? filter, string? category, string? sortColumn, int pageNumber = 1, int pageSize = 10);
-        Task<(bool IsSuccess, ProductDTO? Product, string? ErrorMessage)> GetProductByIdAsync(int id);
-        Task<(bool IsSuccess, ProductSummaryDTO? ProductSummary, string? ErrorMessage)> GetProductSummaryByIdAsync(int id);
+        //Task<(bool IsSuccess, IEnumerable<ProductDTO>? Products, string? ErrorMessage)> GetProductsAsync();
+        //Task<(bool IsSuccess, IEnumerable<ProductSummaryDTO>? ProductSummaries, string? ErrorMessage)> GetProductSummariesAsync();
+        //Task<(bool IsSuccess, IEnumerable<ProductDTO>? Products, PaginationMetadata? PagingData, DateTime? FetchTime, string? ErrorMessage)> GetPagedAndFilteredProductsAsync(string? filter, string? category, string? sortColumn, int pageNumber = 1, int pageSize = 10);
+        //Task<(bool IsSuccess, IEnumerable<ProductSummaryDTO>? Products, PaginationMetadata? PagingData, DateTime? FetchTime, string? ErrorMessage)> GetPagedAndFilteredProductSummariesAsync(string? filter, string? category, string? sortColumn, int pageNumber = 1, int pageSize = 10);
+        //Task<(bool IsSuccess, ProductDTO? Product, string? ErrorMessage)> GetProductByIdAsync(int id);
+        //Task<(bool IsSuccess, ProductSummaryDTO? ProductSummary, string? ErrorMessage)> GetProductSummaryByIdAsync(int id);
         Task<(bool IsSuccess, string? ErrorMessage)> ThrowExceptionForTestingAsync(ThrowExceptionDTO throwExceptionDTO, CancellationToken cancellationToken);
     }
 }
--- Consumer.Blazor/Consumer.Blazor/DownstreamApiServices/ProductsReadApiService.cs
+++ Consumer.Blazor/Consumer.Blazor/DownstreamApiServices/ProductsReadApiService.cs
@@ -1,10 +1,7 @@
 ﻿using Consumer.Blazor.Client.Abstractions;
-using Consumer.Blazor.Client.DTOs.Products;
 using Consumer.Blazor.Client.DTOs.Products.Test;
 using Consumer.Blazor.Client.ErrorHandling;
-using Consumer.Blazor.Client.Paging;
 using Consumer.Blazor.Client.Utility;
-using Consumer.Blazor.HttpServices;
 using Microsoft.Identity.Abstractions;
 using System.Text;
 using System.Text.Json;
@@ -51,192 +48,192 @@ namespace Consumer.Blazor.DownstreamApiServices
         //    }
         //}
 
-        public async Task<(bool IsSuccess, IEnumerable<ProductDTO>? Products, string? ErrorMessage)> GetProductsAsync()
-        {
-            string uri = $"{StaticData.ProductsReadApiService_ProductsPath}";
+        //public async Task<(bool IsSuccess, IEnumerable<ProductDTO>? Products, string? ErrorMessage)> GetProductsAsync()
+        //{
+        //    string uri = $"{StaticData.ProductsReadApiService_ProductsPath}";
 
-            var response = await _downstreamApi.CallApiForUserAsync(
-                serviceName: StaticData.ProductsReadApiService_ServiceName,
-                downstreamApiOptionsOverride: options =>
-                {
-                    options.HttpMethod = "GET";
-                    options.RelativePath = uri;
-                    options.ExtraHeaderParameters = new Dictionary<string, string>
-                    {
-                        { "X-Correlation-ID", Guid.NewGuid().ToString() }
-                    };
-                });
+        //    var response = await _downstreamApi.CallApiForUserAsync(
+        //        serviceName: StaticData.ProductsReadApiService_ServiceName,
+        //        downstreamApiOptionsOverride: options =>
+        //        {
+        //            options.HttpMethod = "GET";
+        //            options.RelativePath = uri;
+        //            options.ExtraHeaderParameters = new Dictionary<string, string>
+        //            {
+        //                { "X-Correlation-ID", Guid.NewGuid().ToString() }
+        //            };
+        //        });
 
-            if (response.IsSuccessStatusCode)
-            {
-                string result = await response.Content.ReadAsStringAsync();
-                IEnumerable<ProductDTO>? products = JsonSerializer.Deserialize<IEnumerable<ProductDTO>>(result, _jsonOptions);
-                Console.WriteLine(products);
-                return (true, products, null);
-            }
-            else
-            {
-                string errorMessage = await GetErrorMessageAsync(response);
-                return (false, null, errorMessage);
-            }
-        }
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        string result = await response.Content.ReadAsStringAsync();
+        //        IEnumerable<ProductDTO>? products = JsonSerializer.Deserialize<IEnumerable<ProductDTO>>(result, _jsonOptions);
+        //        Console.WriteLine(products);
+        //        return (true, products, null);
+        //    }
+        //    else
+        //    {
+        //        string errorMessage = await GetErrorMessageAsync(response);
+        //        return (false, null, errorMessage);
+        //    }
+        //}
 
-        public async Task<(bool IsSuccess, IEnumerable<ProductSummaryDTO>? ProductSummaries, string? ErrorMessage)> GetProductSummariesAsync()
-        {
-            string uri = $"{StaticData.ProductsReadApiService_ProductsPath}/summaries";
+        //public async Task<(bool IsSuccess, IEnumerable<ProductSummaryDTO>? ProductSummaries, string? ErrorMessage)> GetProductSummariesAsync()
+        //{
+        //    string uri = $"{StaticData.ProductsReadApiService_ProductsPath}/summaries";
 
-            var response = await _downstreamApi.CallApiForUserAsync(
-                serviceName: StaticData.ProductsReadApiService_ServiceName,
-                downstreamApiOptionsOverride: options =>
-                {
-                    options.HttpMethod = "GET";
-                    options.RelativePath = uri;
-                    options.ExtraHeaderParameters = new Dictionary<string, string>
-                    {
-                        { "X-Correlation-ID", Guid.NewGuid().ToString() }
-                    };
-                });
+        //    var response = await _downstreamApi.CallApiForUserAsync(
+        //        serviceName: StaticData.ProductsReadApiService_ServiceName,
+        //        downstreamApiOptionsOverride: options =>
+        //        {
+        //            options.HttpMethod = "GET";
+        //            options.RelativePath = uri;
+        //            options.ExtraHeaderParameters = new Dictionary<string, string>
+        //            {
+        //                { "X-Correlation-ID", Guid.NewGuid().ToString() }
+        //            };
+        //        });
 
-            if (response.IsSuccessStatusCode)
-            {
-                Console.WriteLine($"SUCCESS GETTING PRODUCT SUMMARIES.");
-                string result = await response.Content.ReadAsStringAsync();
-                IEnumerable<ProductSummaryDTO>? productSummaries = JsonSerializer.Deserialize<IEnumerable<ProductSummaryDTO>>(result, _jsonOptions);
-                foreach (var summary in productSummaries!)
-                {
-                    Console.WriteLine(summary.Name);
-                }
-                return (true, productSummaries, null);
-            }
-            else
-            {
-                string errorMessage = await GetErrorMessageAsync(response);
-                return (false, null, errorMessage);
-            }
-        }
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        Console.WriteLine($"SUCCESS GETTING PRODUCT SUMMARIES.");
+        //        string result = await response.Content.ReadAsStringAsync();
+        //        IEnumerable<ProductSummaryDTO>? productSummaries = JsonSerializer.Deserialize<IEnumerable<ProductSummaryDTO>>(result, _jsonOptions);
+        //        foreach (var summary in productSummaries!)
+        //        {
+        //            Console.WriteLine(summary.Name);
+        //        }
+        //        return (true, productSummaries, null);
+        //    }
+        //    else
+        //    {
+        //        string errorMessage = await GetErrorMessageAsync(response);
+        //        return (false, null, errorMessage);
+        //    }
+        //}
 
-        public async Task<(bool IsSuccess, IEnumerable<ProductDTO>? Products, PaginationMetadata? PagingData, DateTime? FetchTime, string? ErrorMessage)> GetPagedAndFilteredProductsAsync(
-            string? filter, string? category, string? sortColumn, int pageNumber = 1, int pageSize = 10)
-        {
-            string uri = $"{StaticData.ProductsReadApiService_ProductsPath}/paged?filter={filter}&category={category}&sortColumn={sortColumn}&pageNumber={pageNumber}&pageSize={pageSize}";
+        //public async Task<(bool IsSuccess, IEnumerable<ProductDTO>? Products, PaginationMetadata? PagingData, DateTime? FetchTime, string? ErrorMessage)> GetPagedAndFilteredProductsAsync(
+        //    string? filter, string? category, string? sortColumn, int pageNumber = 1, int pageSize = 10)
+        //{
+        //    string uri = $"{StaticData.ProductsReadApiService_ProductsPath}/paged?filter={filter}&category={category}&sortColumn={sortColumn}&pageNumber={pageNumber}&pageSize={pageSize}";
 
-            var response = await _downstreamApi.CallApiForUserAsync(
-                serviceName: StaticData.ProductsReadApiService_ServiceName,
-                downstreamApiOptionsOverride: options =>
-                {
-                    options.HttpMethod = "GET";
-                    options.RelativePath = uri;
-                    options.ExtraHeaderParameters = new Dictionary<string, string>
-                    {
-                        { "X-Correlation-ID", Guid.NewGuid().ToString() }
-                    };
-                });
+        //    var response = await _downstreamApi.CallApiForUserAsync(
+        //        serviceName: StaticData.ProductsReadApiService_ServiceName,
+        //        downstreamApiOptionsOverride: options =>
+        //        {
+        //            options.HttpMethod = "GET";
+        //            options.RelativePath = uri;
+        //            options.ExtraHeaderParameters = new Dictionary<string, string>
+        //            {
+        //                { "X-Correlation-ID", Guid.NewGuid().ToString() }
+        //            };
+        //        });
 
-            if (response.IsSuccessStatusCode)
-            {
-                string result = await response.Content.ReadAsStringAsync();
-                PagedProductsDTO? pagedProducts = JsonSerializer.Deserialize<PagedProductsDTO>(result, _jsonOptions);
-                // Console.WriteLine(pagedProducts);
-                return (true, pagedProducts?.Products, pagedProducts?.PagingData, pagedProducts?.FetchTime, null);
-            }
-            else
-            {
-                string errorMessage = await GetErrorMessageAsync(response);
-                return (false, null, null, null, errorMessage);
-            }
-        }
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        string result = await response.Content.ReadAsStringAsync();
+        //        PagedProductsDTO? pagedProducts = JsonSerializer.Deserialize<PagedProductsDTO>(result, _jsonOptions);
+        //        // Console.WriteLine(pagedProducts);
+        //        return (true, pagedProducts?.Products, pagedProducts?.PagingData, pagedProducts?.FetchTime, null);
+        //    }
+        //    else
+        //    {
+        //        string errorMessage = await GetErrorMessageAsync(response);
+        //        return (false, null, null, null, errorMessage);
+        //    }
+        //}
 
-        public async Task<(bool IsSuccess, IEnumerable<ProductSummaryDTO>? Products, PaginationMetadata? PagingData, DateTime? FetchTime, string? ErrorMessage)> GetPagedAndFilteredProductSummariesAsync(
-            string? filter, string? category, string? sortColumn, int pageNumber = 1, int pageSize = 10)
-        {
-            string uri = $"{StaticData.ProductsReadApiService_ProductsPath}/paged/summaries?filter={filter}&category={category}&sortColumn={sortColumn}&pageNumber={pageNumber}&pageSize={pageSize}";
+        //public async Task<(bool IsSuccess, IEnumerable<ProductSummaryDTO>? Products, PaginationMetadata? PagingData, DateTime? FetchTime, string? ErrorMessage)> GetPagedAndFilteredProductSummariesAsync(
+        //    string? filter, string? category, string? sortColumn, int pageNumber = 1, int pageSize = 10)
+        //{
+        //    string uri = $"{StaticData.ProductsReadApiService_ProductsPath}/paged/summaries?filter={filter}&category={category}&sortColumn={sortColumn}&pageNumber={pageNumber}&pageSize={pageSize}";
 
-            var response = await _downstreamApi.CallApiForUserAsync(
-               serviceName: StaticData.ProductsReadApiService_ServiceName,
-               downstreamApiOptionsOverride: options =>
-               {
-                   options.HttpMethod = "GET";
-                   options.RelativePath = uri;
-                   options.ExtraHeaderParameters = new Dictionary<string, string>
-                   {
-                        { "X-Correlation-ID", Guid.NewGuid().ToString() }
-                   };
-               });
+        //    var response = await _downstreamApi.CallApiForUserAsync(
+        //       serviceName: StaticData.ProductsReadApiService_ServiceName,
+        //       downstreamApiOptionsOverride: options =>
+        //       {
+        //           options.HttpMethod = "GET";
+        //           options.RelativePath = uri;
+        //           options.ExtraHeaderParameters = new Dictionary<string, string>
+        //           {
+        //                { "X-Correlation-ID", Guid.NewGuid().ToString() }
+        //           };
+        //       });
 
-            if (response.IsSuccessStatusCode)
-            {
-                string result = await response.Content.ReadAsStringAsync();
-                PagedProductSummariesDTO? pagedProductSummaries = JsonSerializer.Deserialize<PagedProductSummariesDTO>(result, _jsonOptions);
-                // Console.WriteLine(pagedProductSummaries);
-                return (true, pagedProductSummaries?.ProductSummaries, pagedProductSummaries?.PagingData, pagedProductSummaries?.FetchTime, null);
-            }
-            else
-            {
-                string errorMessage = await GetErrorMessageAsync(response);
-                return (false, null, null, null, errorMessage);
-            }
-        }
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        string result = await response.Content.ReadAsStringAsync();
+        //        PagedProductSummariesDTO? pagedProductSummaries = JsonSerializer.Deserialize<PagedProductSummariesDTO>(result, _jsonOptions);
+        //        // Console.WriteLine(pagedProductSummaries);
+        //        return (true, pagedProductSummaries?.ProductSummaries, pagedProductSummaries?.PagingData, pagedProductSummaries?.FetchTime, null);
+        //    }
+        //    else
+        //    {
+        //        string errorMessage = await GetErrorMessageAsync(response);
+        //        return (false, null, null, null, errorMessage);
+        //    }
+        //}
 
-        public async Task<(bool IsSuccess, ProductDTO? Product, string? ErrorMessage)> GetProductByIdAsync(int id)
-        {
-            string uri = $"{StaticData.ProductsReadApiService_ProductsPath}/{id}";
+        //public async Task<(bool IsSuccess, ProductDTO? Product, string? ErrorMessage)> GetProductByIdAsync(int id)
+        //{
+        //    string uri = $"{StaticData.ProductsReadApiService_ProductsPath}/{id}";
 
-            var response = await _downstreamApi.CallApiForUserAsync(
-               serviceName: StaticData.ProductsReadApiService_ServiceName,
-               downstreamApiOptionsOverride: options =>
-               {
-                   options.HttpMethod = "GET";
-                   options.RelativePath = uri;
-                   options.ExtraHeaderParameters = new Dictionary<string, string>
-                   {
-                        { "X-Correlation-ID", Guid.NewGuid().ToString() }
-                   };
-               });
+        //    var response = await _downstreamApi.CallApiForUserAsync(
+        //       serviceName: StaticData.ProductsReadApiService_ServiceName,
+        //       downstreamApiOptionsOverride: options =>
+        //       {
+        //           options.HttpMethod = "GET";
+        //           options.RelativePath = uri;
+        //           options.ExtraHeaderParameters = new Dictionary<string, string>
+        //           {
+        //                { "X-Correlation-ID", Guid.NewGuid().ToString() }
+        //           };
+        //       });
 
-            if (response.IsSuccessStatusCode)
-            {
-                ProductDTO? product = await response.Content.ReadFromJsonAsync<ProductDTO>();
-                //string result = await response.Content.ReadAsStringAsync();
-                //ProductDTO? product = JsonSerializer.Deserialize<ProductDTO>(result, _jsonOptions);
-                //Console.WriteLine(product);
-                return (true, product, null);
-            }
-            else
-            {
-                string errorMessage = await GetErrorMessageAsync(response);
-                return (false, null, errorMessage);
-            }
-        }
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        ProductDTO? product = await response.Content.ReadFromJsonAsync<ProductDTO>();
+        //        //string result = await response.Content.ReadAsStringAsync();
+        //        //ProductDTO? product = JsonSerializer.Deserialize<ProductDTO>(result, _jsonOptions);
+        //        //Console.WriteLine(product);
+        //        return (true, product, null);
+        //    }
+        //    else
+        //    {
+        //        string errorMessage = await GetErrorMessageAsync(response);
+        //        return (false, null, errorMessage);
+        //    }
+        //}
 
-        public async Task<(bool IsSuccess, ProductSummaryDTO? ProductSummary, string? ErrorMessage)> GetProductSummaryByIdAsync(int id)
-        {
-            string uri = $"{StaticData.ProductsReadApiService_ProductsPath}/summary/{id}";
+        //public async Task<(bool IsSuccess, ProductSummaryDTO? ProductSummary, string? ErrorMessage)> GetProductSummaryByIdAsync(int id)
+        //{
+        //    string uri = $"{StaticData.ProductsReadApiService_ProductsPath}/summary/{id}";
 
-            var response = await _downstreamApi.CallApiForUserAsync(
-               serviceName: StaticData.ProductsReadApiService_ServiceName,
-               downstreamApiOptionsOverride: options =>
-               {
-                   options.HttpMethod = "GET";
-                   options.RelativePath = uri;
-                   options.ExtraHeaderParameters = new Dictionary<string, string>
-                   {
-                        { "X-Correlation-ID", Guid.NewGuid().ToString() }
-                   };
-               });
+        //    var response = await _downstreamApi.CallApiForUserAsync(
+        //       serviceName: StaticData.ProductsReadApiService_ServiceName,
+        //       downstreamApiOptionsOverride: options =>
+        //       {
+        //           options.HttpMethod = "GET";
+        //           options.RelativePath = uri;
+        //           options.ExtraHeaderParameters = new Dictionary<string, string>
+        //           {
+        //                { "X-Correlation-ID", Guid.NewGuid().ToString() }
+        //           };
+        //       });
 
-            if (response.IsSuccessStatusCode)
-            {
-                string result = await response.Content.ReadAsStringAsync();
-                ProductSummaryDTO? productSummary = JsonSerializer.Deserialize<ProductSummaryDTO>(result, _jsonOptions);
-                Console.WriteLine(productSummary);
-                return (true, productSummary, null);
-            }
-            else
-            {
-                string errorMessage = await GetErrorMessageAsync(response);
-                return (false, null, errorMessage);
-            }
-        }
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        string result = await response.Content.ReadAsStringAsync();
+        //        ProductSummaryDTO? productSummary = JsonSerializer.Deserialize<ProductSummaryDTO>(result, _jsonOptions);
+        //        Console.WriteLine(productSummary);
+        //        return (true, productSummary, null);
+        //    }
+        //    else
+        //    {
+        //        string errorMessage = await GetErrorMessageAsync(response);
+        //        return (false, null, errorMessage);
+        //    }
+        //}
 
         // Dev Tests
         public async Task<(bool IsSuccess, string? ErrorMessage)> ThrowExceptionForTestingAsync(ThrowExceptionDTO throwExceptionDTO, CancellationToken cancellationToken)
