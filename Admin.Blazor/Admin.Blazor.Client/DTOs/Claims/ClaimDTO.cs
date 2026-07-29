d84b79dc005381465ba023f34ce0315e824a71e3
Timothy Riordan <tmiker2004@yahoo.com>
2026-07-29 01:21:22 -0500

    Started universal claims page for API resources in Admin client.

---
+++ Admin.Blazor/Admin.Blazor.Client/DTOs/Claims/ClaimDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Admin.Blazor.Client.DTOs.Claims
+{
+    public class ClaimDTO
+    {
+        public string Type { get; set; }
+        public string Value { get; set; }
+
+        public ClaimDTO(string type, string value)
+        {
+            Type = type;
+            Value = value;
+        }
+    }
+}
