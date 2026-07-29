7ba2760f249bcb957b04a86802865870f5bc0827
Timothy Riordan <tmiker2004@yahoo.com>
2026-07-28 22:45:11 -0500

    Admin and Consumer Client Claims:  Blazor Server Claims now in table with paging, filter, and sort.

--- Consumer.Blazor/Consumer.Blazor.Client/Paging/PaginationMetadata.cs
+++ Consumer.Blazor/Consumer.Blazor.Client/Paging/PaginationMetadata.cs
@@ -6,5 +6,13 @@
         public int TotalPageCount { get; set; }
         public int PageSize { get; set; }
         public int CurrentPage { get; set; }
+
+        public PaginationMetadata(int totalItemCount, int pageSize, int currentPage)
+        {
+            TotalItemCount = totalItemCount;
+            PageSize = pageSize;
+            CurrentPage = currentPage;
+            TotalPageCount = (int)Math.Ceiling(totalItemCount / (double)pageSize);
+        }
     }
 }
