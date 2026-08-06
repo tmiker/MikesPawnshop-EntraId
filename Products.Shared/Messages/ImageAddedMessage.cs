7d3d23b9fc03cfc81e3b2d55ca956753a95ab432
Timothy Riordan <tmiker2004@yahoo.com>
2026-08-06 17:11:59 -0500

    delint

--- Products.Shared/Messages/ImageAddedMessage.cs
+++ Products.Shared/Messages/ImageAddedMessage.cs
@@ -9,13 +9,13 @@ namespace Products.Shared.Messages
         public string AggregateType { get; init; } = default!;
         public int AggregateVersion { get; init; }
         public DateTime OccurredAt { get; init; }
-        public string? CorrelationId { get; init; } ;
+        public string? CorrelationId { get; init; } 
         // command data
-        public string? Name { get; init; } ;
-        public string? Caption { get; init; } ;
+        public string? Name { get; init; } 
+        public string? Caption { get; init; } 
         public int SequenceNumber { get; init; }
-        public string? ImageUrl { get; init; } ;
-        public string? ThumbnailUrl { get; init; } ;
+        public string? ImageUrl { get; init; } 
+        public string? ThumbnailUrl { get; init; } 
 
         public ImageAddedMessage(Guid aggregateId, string aggregateType, int aggregateVersion,
             string? correlationId, string name, string caption, int sequenceNumber,
